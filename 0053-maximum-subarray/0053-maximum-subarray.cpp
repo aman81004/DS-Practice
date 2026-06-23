bd67d40ec9cdc34e3833ec1b64fe517ec1a77a15
SHAIKH MOHAMMED FAZLUR RAHMAN <shaikhmohammedfazlurrahman@gmail.com>
2026-06-23 16:46:18 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.38%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size() ;
+        int sum = 0 ;
+        int maxm = INT_MIN ;
+
+        for( int i = 0 ; i < n ; i++ ){
+            sum += nums[i] ;
+
+            if( sum > maxm ) maxm = sum ;
+
+            if( sum < 0 ) sum = 0 ;
+        }
+        
+        return maxm ;
+    }
+};
