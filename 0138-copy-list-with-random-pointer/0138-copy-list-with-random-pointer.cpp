206a4869e16e78df9a71eed703c26fe6b9916151
SHAIKH MOHAMMED FAZLUR RAHMAN <shaikhmohammedfazlurrahman@gmail.com>
2026-07-25 12:12:07 +0530

    Time: 7 ms (51.79%), Space: 15.1 MB (47%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+
+        // step 1 => create the deep copy without random pointer.
+        Node* dummy =new Node(0);
+        Node* temp = head;
+        Node* tempC = dummy;
+        while(temp){
+            Node* a = new Node(temp->val);
+            tempC->next = a;
+            temp = temp->next;
+            tempC = tempC->next;
+        }
+
+        Node* b = dummy->next;
+        Node* a = head ;
+
+        // Step 2 : Make a map of < original , duplicate > 
+
+        unordered_map< Node* , Node* > m ;
+        Node* tempa = a ;
+        Node* tempb = b ;
+
+        while( tempa != NULL ){
+            m[tempa] = tempb ;
+            tempa = tempa -> next ;
+            tempb = tempb -> next ;
+        }
+
+        for ( auto x : m ){
+            Node* o = x.first ;
+            Node* d = x.second ;
+
+            if( o->random != NULL ) {
+                Node* oRandom = o->random ;
+                Node* dRandom = m[o->random] ;
+                d->random = dRandom ;
+            }
+        }
+
+        return b ;
+        
+
+    }
+};
