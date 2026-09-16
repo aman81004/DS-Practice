0822218286dacc5f644475efe104994e323a53c4
SHAIKH MOHAMMED FAZLUR RAHMAN <shaikhmohammedfazlurrahman@gmail.com>
2026-09-16 23:02:34 +0530

    Time: 420 ms (80.17%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,12 +1,5 @@
 # Write your MySQL query statement below
-SELECT 
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
-FROM 
-    Person p
-LEFT JOIN 
-    Address a 
-ON 
-    p.personId = a.personId;
+Select p.firstName , p.lastName , a.city , a.state 
+From Person p 
+Left Join Address a 
+On p.personId = a.personId
